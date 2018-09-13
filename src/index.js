d93d8f249b5fade4df9e7f5d7fe9041b4d21e905
Alessandra Valenti <alevalenti44@gmail.com>
2018-09-13 12:23:51 +0100

    Properly commented code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+//The shaking animation
 import { ShakeSlow, ShakeHard } from 'reshake';
-
+//The fade in for text
 import { Motion, spring } from 'react-motion';
 
 class Main extends React.Component {
     state = {
+        //array of possible outcome answers
         answers: [
             'It is certain',
             'It is decidedly so',
@@ -29,19 +31,32 @@ class Main extends React.Component {
             'Outlook not so good',
             'Very doubtful',
         ],
+        //The users answer:
         userOutcome: '',
+        //checks if button is clicked:
         clicked: false,
+        //The users question:
         userQuestion: '',
-        errorStyle: '',
+        //The error message that will appear if there is no question
+        errorMessage: '',
     };
 
+    //Handles changes to input field
     handleChange = input => {
         this.setState({ userQuestion: input });
     };
 
+
+    //Handles onClick event
     getReply = () => {
 
+        /*If the users questions contains more than one character,
+         print out the outcome if not show error message for 4 secs*/
+
         if (this.state.userQuestion.length > 1) {
+
+            /*Picks random answer from this.state.answers array*/
+
             let randomNum = Math.floor(Math.random() * 19) + 1;
             let reply = this.state.answers[randomNum];
             console.log(reply);
@@ -52,13 +67,16 @@ class Main extends React.Component {
 
             console.log('is clicked?' + this.state.clicked);
 
+            /* Makes users question disappear after 4 seconds after ball shakes*/
             setTimeout(() => {
                 this.setState({ clicked: false, userQuestion: '' });
             }, 4000);
 
         } else {
+
             this.setState({
-                errorStyle: (
+                /*Creates array if input field is empty*/
+                errorMessage: (
                     <p style={{
                         color: 'red',
                         fontSize: 15,
@@ -72,8 +90,9 @@ class Main extends React.Component {
             });
         }
 
+        /*Makes error message disappear after 3 seconds */
         setTimeout(() => {
-            this.setState({ errorStyle: '' });
+            this.setState({ errorMessage: '' });
         }, 3000);
 
     };
@@ -81,10 +100,13 @@ class Main extends React.Component {
     render() {
         return (
             <div>
+                {/*Makes ball do shake animation*/}
                 <ShakeSlow h={30} v={1} dur={1000} freez={true} fixedStop={true}>
                     <div className="eight-ball">
                         <div className="sheen" />
                         <div className="viewer">
+
+                            {/*Makes text fade in and out*/}
                             <Motion
                                 style={{
                                     currentOpacity: spring(this.state.clicked ? 1 : 0, {
@@ -107,6 +129,7 @@ class Main extends React.Component {
                                                 opacity: currentOpacity
                                             }}
                                         >
+                                            {/*Sets <p> as the userOutcome*/}
                                             {this.state.userOutcome}
                                         </p>
                                     </div>
@@ -121,6 +144,8 @@ class Main extends React.Component {
                     className="questionForm"
                     style={{ display: 'flex', justifyContent: 'center' }}
                 >
+
+                    {/*Gets value of input field calling it e*/}
                     <input
                         className="inputGroup"
                         name="userQuestion"
@@ -129,11 +154,13 @@ class Main extends React.Component {
                         type="text"
                     />
 
+
                     <button className="inputButton" onClick={this.getReply}>
                         Ask!
                     </button>
                 </div>
-                <p>{this.state.errorStyle}</p>
+
+                <p>{this.state.errorMessage}</p>
 
             </div>
         );
